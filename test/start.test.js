c55fa2b690194b8b90acf9d6f9ad02bb666cf334
Naissa Scott <naissas@gmail.com>
2015-10-03 09:40:57 -0700

    test

--- test/start.test.js
+++ test/start.test.js
@@ -5,6 +5,15 @@ var p = require('../lib/p')()
 describe('Suite one', function(){
   it("equals", function () {
       var p = [1,2,3];
-      expect(1).to.equal(1);
+        expect(1).to.equal(1);
+        expect(2).to.equal(2);
+  });
+});
+
+describe('Suite one', function(){
+  it("equals", function () {
+      var p = [1,2,3];
+        expect(1).to.equal(1);
+        expect(2).to.equal(2);
   });
 });
