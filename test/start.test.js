f3607326658b184a79821419cb6f1a59a88a147d
Naissa Scott <naissas@gmail.com>
2015-10-09 14:19:52 -0700

    config.json

--- test/start.test.js
+++ test/start.test.js
@@ -1,46 +1,38 @@
-var expect    = require("chai").expect;
-var request = require("request");
-var p = require('../lib/p')()
+var expect = require("chai").expect;
+var assert = require("assert");
+var getP = require("../lib/getP");
+var fs = require("fs");
+var configFile = process.cwd() + "/config.json";
 
-describe('Suite one', function(){
-  it("equals", function () {
-      var p = [1,2,3];
-        expect(1).to.equal(1);
-        expect(2).to.equal(2);
-  });
-});
+function writeConfig(config, callback) {
+    fs.writeFile(configFile, JSON.stringify(config), callback);
+}
 
-describe('Suite one', function(){
-  it("equals", function () {
-      var p = [1,2,3];
-        expect(1).to.equal(1);
-        expect(2).to.equal(2);
-  });
-});
-
-
-describe('Suite one', function(){
-  it("equals", function () {
-      var p = [1,2,3];
-        expect(1).to.equal(1);
-        expect(2).to.equal(2);
-  });
-});
-
-
-describe('Suite one', function(){
-  it("equals", function () {
-      var p = [1,2,3];
-        expect(1).to.equal(1);
-        expect(2).to.equal(2);
-  });
-});
-
-
-describe('Suite one', function(){
-  it("equals", function () {
-      var p = [1,2,3];
-        expect(1).to.equal(1);
-        expect(2).to.equal(2);
-  });
+describe("getPalette", function () {
+    var config = {};
+    
+    before (function (done) {
+        fs.readFile(configFile, function (err, contents) { 
+           config = JSON.parse(contents.toString());
+           done();
+        });
+    });
+    
+    it ("should throw an error if the result is not an array", function (done) {
+        writeConfig({palette: "string"}, function (err) {
+            assert.throws(getP, "is not an array");
+            done();
+        });
+    });
+    
+    it ("should throw an error if the result is not an array", function (done) {
+         writeConfig({palette: "string"}, function (err) {
+             assert.throws(getP, "is not an array");
+             done();
+         });
+     });
+     
+    afterEach ( function (done) {
+        writeConfig(config, done);
+    });
 });
