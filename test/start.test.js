558dbb0d9dca2564c51fcff1847b5358da2f9a18
Naissa Scott <naissas@gmail.com>
2015-10-10 20:52:15 -0700

    test

--- test/start.test.js
+++ test/start.test.js
@@ -62,3 +62,13 @@ describe('Suite one', function(){
 });
 
 
+describe('Suite one', function(){
+  it("equals", function () {
+      var p = [1,2,3];
+        expect(1).to.equal(1);
+        expect(2).to.equal(2);
+  });
+});
+
+
+
