3a224657d091572854c71eb698ccdee60a830976
Weina Scott <weina@wepay.com>
2015-10-12 19:31:45 -0700

    test

--- test/start.test.js
+++ test/start.test.js
@@ -15,3 +15,12 @@ describe('Suite one', function(){
         expect(2).to.equal(2);
   });
 });
+
+
+describe('Suite one', function(){
+  it("equals", function () {
+      var p = [1,2,3];
+        expect(1).to.equal(1);
+        expect(2).to.equal(2);
+  });
+});
